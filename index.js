2f177adb658b57ff4eebcdb29940d0e1e179a646
Jon <jonmwood@gmail.com>
2021-02-25 12:15:37 -0800

    add log messages with log-symbols package

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const italic = chalk.italic
 const twitterColor = chalk.hex(`#1da1f2`).bold.inverse
 const githubColor = chalk.hex(`#6cc644`).bold.inverse
 
+// Alerts
+const sym = require('log-symbols')
+const success = chalk.green.inverse
+const info = chalk.blue.inverse
+const warning = chalk.keyword('orange').inverse
+const error = chalk.red.bold.inverse
+
 welcome({
 	title: `Jon Wood`,
 	tagLine: `Welcome to this cli!`,
@@ -25,4 +32,13 @@ log(`${italic(`Bio: hello, nice to meet you!!!`)}
 ${twitterColor(` Twitter `)} ${dim(`https://twitter.com/jonmwood`)}
 ${githubColor(` Github `)} ${dim(`https://github.com/jonmwood`)}
 
+`)
+console.log(`
+${sym.success} ${success(` SUCCESS `)}: Thanks for checking out my CLI.
+
+${sym.info} ${info(` INFO `)}: I'm creating a course on NODECLI.com
+
+${sym.warning} ${warning(` WARNING `)}: Please don't copy me.
+
+${sym.error} ${error(` ERROR `)}: This is an error message!!!!
 `)
